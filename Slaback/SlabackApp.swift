fe410806103ec2b84f6ccbfe3609ab38941c29f1
Juan David Lizarazo Ferro <juan_lizarazoferro@brown.edu>
2026-03-10 17:40:26 -0500

    Initial Commit

---
+++ Slaback/SlabackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlabackApp.swift
+//  Slaback
+//
+//  Created by Ivory Tower on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct SlabackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
